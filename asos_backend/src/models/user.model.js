6f5a0cdf004f7d4fc355e4d684aedc338271fffe
jain-hemant <hemantjain1321@gmail.comcls>
2025-03-18 08:45:40 +0530

    user model and hasing module created

---
+++ asos_backend/src/models/user.model.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        unique: true, // Ensure email is unique
+        lowercase: true,
+        trim: true,
+        match: [/.+@.+\..+/, 'Please provide a valid email address'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: [6, 'Password must be at least 6 characters'],
+        select: false
+    },
+    phone: {
+        type: String,
+        require: true,
+        minlength: 10,
+        maxLength: 10
+    },
+    gender: { type: String, require: true },
+    address: {
+        country: { type: String, require: true },
+        state: { type: String, require: true },
+        district: { type: String, require: true },
+        city: { type: String, require: true },
+        zip: { type: String, require: true },
+        latitude: { type: String },
+        longitude: { type: String },
+    },
+    createdAt: { type: Date, default: Date.now },
+}, { versionKey: false })
+
+const UserModel = mongoose.model("user", UserSchema)
+
+module.exports = UserModel
---
+++ asos_backend/src/utils/crypto.js
@@ -0,0 +1,23 @@
+const brcypt = require("bcrypt")
+const { SALT_ROUND } = require("../config/config")
+
+const generateHash = async (data) => {
+    try {
+        const isHashed = await brcypt.hash(data, SALT_ROUND)
+        return isHashed
+    } catch (error) {
+        console.log(error);
+
+    }
+}
+
+const verifyHash = async (userData, hashData) => {
+    try {
+        const isVerified = await brcypt.compare(userData, hashData)
+        return isVerified
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = { generateHash, verifyHash }
