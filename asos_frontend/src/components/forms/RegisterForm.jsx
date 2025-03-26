3e053b3d57d018ad3d996c8cf18e572130aa424a
jain-hemant <hemantjain1321@gmail.comcls>
2025-03-26 23:19:48 +0530

    Enterprise-Level Structure (Micro-Frontend Ready) created All Directory and Files

---
+++ asos_frontend/src/components/forms/RegisterForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const RegisterForm = () => {
+    return (
+        <div>
+            <form>
+                
+            </form>
+        </div>
+    )
+}
+
+export default RegisterForm
